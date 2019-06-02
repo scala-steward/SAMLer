27be0aaeee4a9903a9f5e0f1423af8cb3251cbba
Henri Cook <github@henricook.com>
2019-06-02 21:01:04 +0100

    Working authorize with sample data

--- app/repositories/ClientRepository.scala
+++ app/repositories/ClientRepository.scala
@@ -5,5 +5,6 @@ import model.Client
 import scalaz.zio.Task
 
 class ClientRepository @Inject()() {
-  def getClient(clientId: String): Task[Client] = ???
+  def getClient(clientId: String): Task[Client] =
+    Task.succeed(Client(clientId, "Test Client 1"))
 }
