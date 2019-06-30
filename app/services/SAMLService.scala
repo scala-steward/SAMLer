2a35fc27cecadb561e3ee009f7f489add319b90b
Henri Cook <github@henricook.com>
2019-06-30 12:51:39 +0100

    Add favicon

--- app/services/SAMLService.scala
+++ app/services/SAMLService.scala
@@ -5,13 +5,14 @@ import java.util.Base64
 
 import javax.inject.Inject
 import model.AuthnRequest
+import play.api.{Logger, Logging}
 import repositories.{ClientRepository, IdpRepository}
 import zio.Task
 
 class SAMLService @Inject()(
     clientRepository: ClientRepository,
     idpRepository: IdpRepository
-) {
+) extends Logging {
   def buildAuthnRequest(
       clientId: String,
       idpId: String,
@@ -26,6 +27,7 @@ class SAMLService @Inject()(
       client <- clientRepository.getClient(clientId)
       idp <- idpRepository.getIdp(client.id, idpId)
       authnRequest = mkAuthnRequest()
+      _ = logger.warn("=== Authn contents = " + authnRequest)
     } yield
       AuthnRequest(
         client.name,
